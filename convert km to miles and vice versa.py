e2a89e9da849c5485cf7a5635784b3dde4ef294d
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-06-04 14:27:36 +0530

    Create convert km to miles and vice versa.py

---
+++ convert km to miles and vice versa.py
@@ -0,0 +1,13 @@
+#Python program to convert kilometers to miles...
+# 1 kilometer = 0.62137 miles 
+
+# Miles = kilometer * 0.62137
+Km = float(input("Enter Number in Km :"))
+Miles = Km * 0.62137   #formula
+print("%0.3f Km is equal to %0.3f Miles"%(Km,Miles))
+
+#Miles to Kilometers...
+# Kilometer = miles / 0.62137
+miles = float(input("Enter Number in Miles :"))
+Km = Miles / 0.62137   #formula
+print("%0.3f Miles is equal to %0.3f km"%(Miles,Km))
